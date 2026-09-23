b7976c6c37d43a71415655b33ae4c689cfbe5e86
kavyashah16 <shahkavya260@gmail.com>
2026-09-23 18:35:17 +0530

    feat: update SSL configuration for database connection and change API URL to production

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,9 +3,14 @@ dotenv.config();
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 
+import fs from "fs";
+import path from "path";
+
 const pool = mysql.createPool({
   uri: process.env.DATABASE_URL!,
-  ssl: { rejectUnauthorized: true },
+  ssl: {
+    ca: fs.readFileSync(path.join(process.cwd(), "certs/aiven-ca.pem")),
+  },
 });
 
 export const db = drizzle({ client: pool });
